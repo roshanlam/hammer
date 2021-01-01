aed66f6b5731ee1f63313942bba0b7fd6790e17f
Roshan Lamichhane <lamichhaner40@gmail.com>
2021-01-01 15:31:49 -0500

    Updated user.py, need to test the code.

--- hammer/user.py
+++ hammer/user.py
@@ -1,29 +1,36 @@
 class User:
-    def __init__(
-        URL = None,
-    ):
+    def __init__(self, name, username, password):
+        self.name = name
+        self.username = name
+        self.password = password
+        self.loggedIn = True
+    
+    def home(user):
+        print("Login, Register")
+        user_input = input("What would you like to do:\n")
+        if(user_input == "register" or user_input == "Register"):
+            register()
+        elif(user_input == "login" or user_input == "Login"):
+            login(user)
+        else:
+            print("Choose a valid option")
+            home('')
+    
+    def register():
+        name = input("Name:\n")
+        username = input("Username:\n")
+        password = input("Password:\n")
+        userr = User(name, username, password)
+        print("Welcome, " + username.name)
+        home(username)
 
-def saveQuery(self):
-	"""
-	Saves the users query for later use, asks the user first though :)
-	"""
-null = 0
-return null
-
-
-def loginSignUp(self):
-	self.name = ""
-	self.password = ""
-
-null = 0
-
-return null
-
-def encrypt():
-
-	"""
-	Encrypts User's data and protects it. 
-	"""
-	null = 0
-return null
-
+    def login(user):
+        l1 = input("Username:\n")
+        l2 = input("Password:\n")
+        if(l2 == user.password):
+            print("Welcome,  " + user.name)
+         else:
+            print("Incorrect username or password")
+            login(user)
+          home('')
+    home('')
